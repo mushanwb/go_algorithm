36df89fcbec1ef1567a845279c82ff5a14083d16
bowang <bowang@xiaoe-tech.com>
2023-06-15 20:06:00 +0800

    传值和传引用

--- sort/bubble_sort_test.go
+++ sort/bubble_sort_test.go
@@ -1,6 +1,7 @@
 package sort
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -17,6 +18,31 @@ func bubbleSort(a *[]int) {
 			if (*a)[j] > (*a)[j+1] {
 				(*a)[j] = (*a)[j+1]
 				(*a)[j+1] = temp
+			}
+		}
+	}
+}
+
+/*
+传值和传引用
+*/
+
+func TestBubbleSortV2(t *testing.T) {
+	a := []int{4, 1, 1, 3, 5, 2, 9, 7, 8, 10}
+	fmt.Printf("原始指针的内存地址是1 %p\n", &a)
+	bubbleSortV2(a)
+	fmt.Printf("原始指针的内存地址是3 %p\n", &a)
+	t.Log(a)
+}
+
+func bubbleSortV2(a []int) {
+	fmt.Printf("原始指针的内存地址是2 %p\n", &a)
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(a)-1-i; j++ {
+			temp := a[j]
+			if a[j] > a[j+1] {
+				a[j] = a[j+1]
+				a[j+1] = temp
 			}
 		}
 	}
