206c8fad0855c5cb90539b3932da4a10747c37e1
bowang <bowang@xiaoe-tech.com>
2023-06-16 16:46:45 +0800

    快速排序方案一

--- sort/quick_sort_test.go
+++ sort/quick_sort_test.go
@@ -5,32 +5,30 @@ import (
 )
 
 func TestQuickSort(t *testing.T) {
-	list := []int{2, 44, 4, 8, 33, 1, 22, -11, 6, 34, 55, 54, 9}
-	QuickSort(list, 0, len(list)-1)
-	t.Log(list)
+	list := []int{5, 1, 1, 2, 0, 0}
+	sortList := QuickSortV1(list)
+	t.Log(sortList)
 }
 
-func QuickSort(list []int, low, high int) {
-	if high > low {
-		pivot := partition(list, low, high)
-		QuickSort(list, low, pivot-1)
-		QuickSort(list, pivot+1, high)
+/*
+方式一、采用数组合并的方式(会使用多余的空间，数据量多大运行会出问题)
+*/
+func QuickSortV1(list []int) (sortList []int) {
+	if len(list) < 2 {
+		return list
 	}
-}
-
-func partition(list []int, low, high int) int {
-	pivot := list[low]
-	for low < high {
-		for low < high && pivot <= list[high] {
-			high--
+	var left []int
+	var right []int
+	flag := list[0]
+	for i, v := range list {
+		if i == 0 {
+			continue
 		}
-		list[low] = list[high]
-		for low < high && pivot >= list[low] {
-			low++
+		if v > flag {
+			right = append(right, v)
+		} else {
+			left = append(left, v)
 		}
-
-		list[high] = list[low]
 	}
-	list[low] = pivot
-	return low
+	return append(append(QuickSortV1(left), flag), QuickSortV1(right)...)
 }
