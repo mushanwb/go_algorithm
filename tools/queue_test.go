30e4799b5706cc5773987a0c7320049c1448a540
bowang <bowang@xiaoe-tech.com>
2023-06-20 17:34:42 +0800

    切片实现队列

---
+++ tools/queue_test.go
@@ -0,0 +1,35 @@
+package tools
+
+import "testing"
+
+type QueueArray []interface{}
+
+func TestQueue(t *testing.T) {
+	var q QueueArray
+	q.Push(1)
+	q.Push(2)
+	t.Log(q.Pop())
+	q.Push(3)
+	t.Log(q.Pop())
+	t.Log(q.Pop())
+	t.Log(q.Pop())
+	t.Log(q.len())
+	t.Log(q)
+}
+
+func (q *QueueArray) Push(data interface{}) {
+	*q = append(*q, data)
+}
+
+func (q *QueueArray) Pop() (data interface{}) {
+	if q.len() == 0 {
+		return nil
+	}
+	data = (*q)[0]
+	*q = (*q)[1:len(*q)]
+	return data
+}
+
+func (q *QueueArray) len() (n int) {
+	return len(*q)
+}
