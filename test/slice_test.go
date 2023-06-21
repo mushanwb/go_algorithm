f550084dd24e5c8494a88a20f7095c3fafb936a9
bowang <bowang@xiaoe-tech.com>
2023-06-21 20:57:12 +0800

    测试切片截取的内存存储

--- test/slice_test.go
+++ test/slice_test.go
@@ -1,6 +1,9 @@
 package test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSlice(t *testing.T) {
 	s := []int{1, 2, 3, 4}
@@ -8,5 +11,18 @@ func TestSlice(t *testing.T) {
 	for i, v := range s {
 		s[i] = v + 1
 	}
+	t.Log(s)
+}
+
+func TestSliceV2(t *testing.T) {
+	s := []int{1, 2, 3, 4}
+	fmt.Printf("Address of i=%d:\t%p\n", s, &s[0])
+	s[0] = 100
+	fmt.Printf("Address of i=%d:\t%p\n", s, &s[0])
+
+	s = s[1:4]
+	fmt.Printf("Address of i=%d:\t%p\n", s, &s[0])
+	s[0] = 3
+	s = append(s, 5)
 	t.Log(s)
 }
