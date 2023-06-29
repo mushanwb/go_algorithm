71f3f3d8d50da31c1694fd375c5af4bd0d5ed929
bowang <bowang@xiaoe-tech.com>
2023-06-29 11:51:26 +0800

    切片说明

--- test/slice_test.go
+++ test/slice_test.go
@@ -32,3 +32,23 @@ func TestSliceV2(t *testing.T) {
 	s = append(s, 5)
 	t.Log(s)
 }
+
+// 测试切片后，在 append
+func TestSliceV3(t *testing.T) {
+	s := []int{1, 2, 3, 4}
+	// 切割 s
+	b := s[0:2]
+	//更改 b 第一个元素,s 也会跟着改
+	b[0] = 2
+	// 打印 s
+	t.Log(s) // [2 2 3 4]
+	t.Log(b) // [2 2]
+	//s 增加一个元素
+	s = append(s, 5)
+	//在更改b中元素
+	b[0] = 3
+	// 打印 s
+	t.Log(s) // [2 2 3 4 5]
+	t.Log(b) // [3 2]
+	// s 没有变，因为 append 会复制另外一个内存，导致 s b 不在共享内存
+}
