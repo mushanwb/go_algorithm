b42de66c0d5157993fffa13a2ba6be0b5d9ff8b7
bowang <bowang@xiaoe-tech.com>
2023-08-22 11:30:01 +0800

    杨辉三角

---
+++ array/yanghui_triangle_test.go
@@ -0,0 +1,31 @@
+package array
+
+import (
+	"testing"
+)
+
+/*
+给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+
+在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+
+*/
+
+func TestYangHuiTriangle(t *testing.T) {
+	numRows := 5
+	result := generate(numRows)
+	t.Log(result)
+}
+
+func generate(numRows int) [][]int {
+	ans := make([][]int, numRows)
+	for i := range ans {
+		ans[i] = make([]int, i+1)
+		ans[i][0] = 1
+		ans[i][i] = 1
+		for j := 1; j < i; j++ {
+			ans[i][j] = ans[i-1][j] + ans[i-1][j-1]
+		}
+	}
+	return ans
+}
